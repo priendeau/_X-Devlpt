8eade84ea686b57e3aa208fe7f1cf428e023e530
Maxiste Deams <maxistedeams@gmail.com>
2010-10-14 21:59:10 -0400

    Project startup, a fastwriter... Will probaly doing an executable for windows If I own permission to install OpenOffice... First reason why it's versatile, I can make an extra hand writing by itself...

---
+++ UnoOOwriterBridgeExample.py
@@ -0,0 +1,103 @@
+import sys, os, re, uno, iterpipes
+from iterpipes import cmd, bincmd, linecmd, run, call, check_call, cmdformat, compose
+
+
+"""
+Openning the OpenOffice Writer with socket Handler
+"""
+
+class OpenOfficeWriterBrigeFactory( object ):
+  IntPortBridge=2002
+  StrHostBridge='localhost'
+  StrAcceptBridgeType='socket'
+  localContext=None
+  resolver=None
+  ctx=None
+  URIConnection=None
+
+  class PropertyGetterURIException( Exception ):
+    Msg='PropertyURIException raised from Function %s.'
+    def __init__(self, value ):
+      Warning.__init__( self, self.Msg % ( value ) )
+
+  class PropertySetterURIException( Exception ):
+    Msg='PropertySetterURIException raised from Function %s.'
+    def __init__(self, value ):
+      Warning.__init__( self, self.Msg % ( value ) )
+
+  class PropertySetterURINoAttrAssingException( Exception ):
+    Msg='PropertySetterURINoAttrAssingException raised from Function %s.'
+    def __init__(self, value ):
+      Warning.__init__( self, self.Msg % ( value ) )
+
+  class PropertySetterURIMissingFieldException( Exception ):
+    Msg='PropertySetterURIMissingFieldException raised from Function %s.'
+    def __init__(self, value ):
+      Warning.__init__( self, self.Msg % ( value ) )
+
+  class PropertySetterUpdateURIBridgeArgListException( Exception ):
+    Msg='PropertySetterUpdateURIBridgeArgListException %s.'
+  
+  class ListURIConnectionNotInstanciated( Warning ):
+    Msg='ListURIConnectionNotInstanciated raised from Function %s in favor of empty self.URIConnection not instantiated or incorrectly parsed.'
+    def __init__(self, value ):
+      Warning.__init__( self, self.Msg % ( value ) )
+
+  def GetUriConnection( self ):
+    if not self.URIConnection  == None:
+      return self.URIConnection
+    else:
+      raise self.PropertyURIException , self.GetUriConnection.__name__
+
+  def SetUriConnection( self, value ):
+    FuncName=self.SetUriConnection.__name__
+    ValueListOrder=[ 'StrAcceptBridgeType','StrHostBridge','IntPortBridge' ]
+    if len( value ) != len( ValueListOrder ):
+      raise self.PropertySetterURIMissingFieldException, FuncName
+    for IntPosN in range( 0 , len(ValueListOrder) ) :
+      StreamInfoValue=value[IntPosN]
+      StreamInfoKey=ValueListOrder[IntPosN]
+      if StreamInfoValue == '':
+        raise self.PropertySetterURIException, FuncName
+      if not hasattr( self, StreamInfoKey ):
+        raise self.PropertySetterURINoAttrAssingException, FuncName
+      getattr( self, StreamInfoKey, StreamInfoValue )
+      self.URIConnection.append( StreamInfoValue )
+
+  def SetUpdateURIBridgeArgList( self, value  ):
+    if value == True :
+      self.URIConnection=[ self.StrAcceptBridgeType , self.StrHostBridge, self.IntPortBridge ]
+    else:
+      raise self.
+
+  def GetUpdateURIBridgeArgList( self ):
+    return self.URIConnection
+
+  PropertyUpdateURIBridge=property( GetUpdateURIBridgeArgList, SetUpdateURIBridgeArgList )
+  
+  def CmdOpenWriter( self ):
+    try:
+      self.AlineCmd=linecmd( 'oowriter {}', "\"-accept=%s,host=%s,port=%s;urp;\"" %  ListArgParse )
+    except self.PropertyURIException:
+      raise (self.PropertyGetterURIException,self.ListURIConnectionNotInstanciated), (self.CmdOpenWriter.__name__,self.CmdOpenWriter.__name__)
+
+    for self.ItemLine in run( self.AlineCmd ):
+      sys.stdout.write( '%s' % ( self.ItemLine )  )
+
+  def OpenBridge( self ):
+    """
+       Creating Context
+    """
+    self.localContext = uno.getComponentContext()
+    """
+      Creating Resolver
+    """
+    self.resolver = localContext.ServiceManager.createInstanceWithContext( "com.sun.star.bridge.UnoUrlResolver", localContext )
+    """
+      Connecting the Bridge
+    """
+    self.ctx = resolver.resolve( "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext" )
+
+
+
+
