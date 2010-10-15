d01f8b72016a0b8653c3d1f6f17cba0fcf03326d
Maxiste Deams <maxistedeams@gmail.com>
2010-10-14 22:23:11 -0400

    commit c768ee13-3a28-411d-b9d0-58b86095304e

--- UnoOOwriterBridgeExample.py
+++ UnoOOwriterBridgeExample.py
@@ -6,116 +6,134 @@ from iterpipes import cmd, bincmd, linecmd, run, call, check_call, cmdformat, co
 Openning the OpenOffice Writer with socket Handler
 """
 
-class OpenOfficeWriterBrigeFactory( object ):
-  IntPortBridge=2002
-  StrHostBridge='localhost'
-  StrAcceptBridgeType='socket'
-  localContext=None
-  resolver=None
-  ctx=None
-  URIConnection=None
-  WriterReady=False
-  InterfaceWaitingTotal=None
-  DefaultSleepSlice=5
-
-  class FactoryFinish( Exception ):
-    Msg='PropertyURIException raised from Function %s.'
-    def __init__(self, value ):
-      Exception.__init__( self, self.Msg % ( value ) )
-
-  class PropertyGetterURIException( Exception ):
-    Msg='PropertyURIException raised from Function %s.'
-    def __init__(self, value ):
-      Exception.__init__( self, self.Msg % ( value ) )
-
-  class PropertySetterURIException( Exception ):
-    Msg='PropertySetterURIException raised from Function %s.'
-    def __init__(self, value ):
-      Exception.__init__( self, self.Msg % ( value ) )
-
-  class PropertySetterURINoAttrAssingException( Exception ):
-    Msg='PropertySetterURINoAttrAssingException raised from Function %s.'
-    def __init__(self, value ):
-      Exception.__init__( self, self.Msg % ( value ) )
-
-  class PropertySetterURIMissingFieldException( Exception ):
-    Msg='PropertySetterURIMissingFieldException raised from Function %s.'
-    def __init__(self, value ):
-      Exception.__init__( self, self.Msg % ( value ) )
-
-  class PropertySetterUpdateURIBridgeArgListException( Exception ):
-    Msg='PropertySetterUpdateURIBridgeArgListException raised from Function %s. Missing the only one key <<True>>'
+class OpenOfficeWriterBrigeImpl( object ):
   
-  class ListURIConnectionNotInstanciated( Warning ):
-    Msg='ListURIConnectionNotInstanciated raised from Function %s in favor of empty self.URIConnection not instantiated or incorrectly parsed.'
-    def __init__(self, value ):
-      Warning.__init__( self, self.Msg % ( value ) )
+  class OpenOfficeWriterBrigeFactory( object ):
+    IntPortBridge=2002
+    StrHostBridge='localhost'
+    StrAcceptBridgeType='socket'
+    localContext=None
+    resolver=None
+    ctx=None
+    URIConnection=None
+    WriterReady=False
+    InterfaceWaitingTotal=None
+    DefaultSleepSlice=5
+    TriggerList={ 'interruption':False }
+    TriggerKey=None
+    TriggerValue=None
 
-  def GetUriConnection( self ):
-    if not self.URIConnection  == None:
+
+    class FactoryFinish( Warning ):
+      Msg='FactoryFinish raised for Interruption of %s'
+      def __init__(self, value ):
+        Exception.__init__( self, self.Msg % ( value ) )
+
+    class PropertyGetterURIException( Exception ):
+      Msg='PropertyURIException raised from Function %s.'
+      def __init__(self, value ):
+        Exception.__init__( self, self.Msg % ( value ) )
+
+    class PropertySetterURIException( Exception ):
+      Msg='PropertySetterURIException raised from Function %s.'
+      def __init__(self, value ):
+        Exception.__init__( self, self.Msg % ( value ) )
+
+    class PropertySetterURINoAttrAssingException( Exception ):
+      Msg='PropertySetterURINoAttrAssingException raised from Function %s.'
+      def __init__(self, value ):
+        Exception.__init__( self, self.Msg % ( value ) )
+
+    class PropertySetterURIMissingFieldException( Exception ):
+      Msg='PropertySetterURIMissingFieldException raised from Function %s.'
+      def __init__(self, value ):
+        Exception.__init__( self, self.Msg % ( value ) )
+
+    class PropertySetterUpdateURIBridgeArgListException( Exception ):
+      Msg='PropertySetterUpdateURIBridgeArgListException raised from Function %s. Missing the only one key <<True>>'
+    
+    class ListURIConnectionNotInstanciated( Warning ):
+      Msg='ListURIConnectionNotInstanciated raised from Function %s in favor of empty self.URIConnection not instantiated or incorrectly parsed.'
+      def __init__(self, value ):
+        Warning.__init__( self, self.Msg % ( value ) )
+
+    def GetTriggerState( self ):
+      return self.TriggerList[self.TriggerKey]
+
+    """
+      Only True are allowed, most of the time they are final interruption and will not imply an unlocking...
+      There is less-sever Trigger-state comming...
+    """
+    def SetTriggerState( self, value ):
+      self.TriggerKey=value 
+      self.TriggerValue=True
+      self.TriggerList[self.TriggerKey]=self.TriggerValue
+
+    def GetUriConnection( self ):
+      if not self.URIConnection  == None:
+        return self.URIConnection
+      else:
+        raise self.PropertyURIException , self.GetUriConnection.__name__
+
+    def SetUriConnection( self, value ):
+      FuncName=self.SetUriConnection.__name__
+      ValueListOrder=[ 'StrAcceptBridgeType','StrHostBridge','IntPortBridge' ]
+      if len( value ) != len( ValueListOrder ):
+        raise self.PropertySetterURIMissingFieldException, FuncName
+      for IntPosN in range( 0 , len(ValueListOrder) ) :
+        StreamInfoValue=value[IntPosN]
+        StreamInfoKey=ValueListOrder[IntPosN]
+        if StreamInfoValue == '':
+          raise self.PropertySetterURIException, FuncName
+        if not hasattr( self, StreamInfoKey ):
+          raise self.PropertySetterURINoAttrAssingException, FuncName
+        getattr( self, StreamInfoKey, StreamInfoValue )
+        self.URIConnection.append( StreamInfoValue )
+
+    def SetUpdateURIBridgeArgList( self, value  ):
+      FuncName=self.SetUpdateURIBridgeArgList.__name__
+      if value == True :
+        self.URIConnection=[ self.StrAcceptBridgeType , self.StrHostBridge, self.IntPortBridge ]
+      else:
+        raise self.PropertySetterUpdateURIBridgeArgListException, FuncName
+
+    def GetUpdateURIBridgeArgList( self ):
       return self.URIConnection
-    else:
-      raise self.PropertyURIException , self.GetUriConnection.__name__
 
-  def SetUriConnection( self, value ):
-    FuncName=self.SetUriConnection.__name__
-    ValueListOrder=[ 'StrAcceptBridgeType','StrHostBridge','IntPortBridge' ]
-    if len( value ) != len( ValueListOrder ):
-      raise self.PropertySetterURIMissingFieldException, FuncName
-    for IntPosN in range( 0 , len(ValueListOrder) ) :
-      StreamInfoValue=value[IntPosN]
-      StreamInfoKey=ValueListOrder[IntPosN]
-      if StreamInfoValue == '':
-        raise self.PropertySetterURIException, FuncName
-      if not hasattr( self, StreamInfoKey ):
-        raise self.PropertySetterURINoAttrAssingException, FuncName
-      getattr( self, StreamInfoKey, StreamInfoValue )
-      self.URIConnection.append( StreamInfoValue )
+    PropertyTriggerInterrupt=property( GetTriggerState, SetTriggerState )
 
-  def SetUpdateURIBridgeArgList( self, value  ):
-    FuncName=self.SetUpdateURIBridgeArgList.__name__
-    if value == True :
-      self.URIConnection=[ self.StrAcceptBridgeType , self.StrHostBridge, self.IntPortBridge ]
-    else:
-      raise self.PropertySetterUpdateURIBridgeArgListException, FuncName
+    PropertyUpdateURIBridge=property( GetUpdateURIBridgeArgList, SetUpdateURIBridgeArgList )
 
-  def GetUpdateURIBridgeArgList( self ):
-    return self.URIConnection
+    PropertyUriConnection=property( GetUriConnection, SetUriConnection )
+    
+    def CmdOpenWriter( self ):
+      try:
+        self.AlineCmd=linecmd( 'oowriter {}', "\"-accept=%s,host=%s,port=%s;urp;\"" %  self.PropertyUriConnection )
+      except self.PropertyURIException:
+        raise (self.PropertyGetterURIException,self.ListURIConnectionNotInstanciated), (self.CmdOpenWriter.__name__,self.CmdOpenWriter.__name__)
+      for self.ItemLine in run( self.AlineCmd ):
+        sys.stdout.write( '%s' % ( self.ItemLine )  )
+      self.WriterReady=True
 
-  PropertyUpdateURIBridge=property( GetUpdateURIBridgeArgList, SetUpdateURIBridgeArgList )
-
-  PropertyUriConnection=property( GetUriConnection, SetUriConnection )
-  
-  def CmdOpenWriter( self ):
-    try:
-      self.AlineCmd=linecmd( 'oowriter {}', "\"-accept=%s,host=%s,port=%s;urp;\"" %  ListArgParse )
-    except self.PropertyURIException:
-      raise (self.PropertyGetterURIException,self.ListURIConnectionNotInstanciated), (self.CmdOpenWriter.__name__,self.CmdOpenWriter.__name__)
-    for self.ItemLine in run( self.AlineCmd ):
-      sys.stdout.write( '%s' % ( self.ItemLine )  )
-    self.WriterReady=True
-
-  def StartThread( self ):
-    thread.start_new_thread( self.CmdOpenWriter, (,) )
-    while not self.WriterReady :
-      time.sleep( self.DefaultSleepSlice )
-      self.InterfaceWaitingTotal+=self.DefaultSleepSlice
-      
-
-  def OpenBridge( self ):
-    """
-       Creating Context
-    """
-    self.localContext = uno.getComponentContext()
-    """
-      Creating Resolver
-    """
-    self.resolver = localContext.ServiceManager.createInstanceWithContext( "com.sun.star.bridge.UnoUrlResolver", localContext )
-    """
-      Connecting the Bridge
-    """
-    self.ctx = resolver.resolve( "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext" )
+    def OpenBridge( self ):
+      """
+         Creating Context
+      """
+      self.localContext = uno.getComponentContext()
+      """
+        Creating Resolver
+      """
+      self.resolver = localContext.ServiceManager.createInstanceWithContext( "com.sun.star.bridge.UnoUrlResolver", localContext )
+      """
+        Connecting the Bridge
+      """
+      self.ctx = resolver.resolve( "uno:%s,host=%s,port=%s;urp;StarOffice.ComponentContext" % self.PropertyUriConnection  )
 
 
+    def StartThread( self ):
+      thread.start_new_thread( self.CmdOpenWriter, (,) )
+      while not self.WriterReady :
+        time.sleep( self.DefaultSleepSlice )
+        self.InterfaceWaitingTotal+=self.DefaultSleepSlice
 
 
