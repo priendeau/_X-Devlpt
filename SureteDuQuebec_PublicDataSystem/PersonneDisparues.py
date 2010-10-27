c09c6a82033bfa697ee56d43d2e1358c4aedbc53
Maxiste Deams <maxistedeams@gmail.com>
2010-10-27 18:31:25 -0400

    updated:UUID(d05ac6fc-0ac8-4f62-9550-a530b5644f99)

--- SureteDuQuebec_PublicDataSystem/PersonneDisparues.py
+++ SureteDuQuebec_PublicDataSystem/PersonneDisparues.py
@@ -1,5 +1,7 @@
 ﻿import pynav
 from pynav import Pynav
+from SureteDuQuebec_PublicDataSystem import decorator
+from SureteDuQuebec_PublicDataSystem.decorator import DecoratorSQ
 
 """
 
@@ -22,37 +24,64 @@ from pynav import Pynav
 
 __KeyDictAttribute__=[ 'Object','url','web','image', 'link' ]
 
-def CreateKeyName( ListName ):
-  for itemKey in __KeyDictAttribute__:
-    setattr( __builtins__,  itemKey, itemKey )
-
-CreateKeyName( __KeyDictAttribute__ )
 
 
-class DictProperty( object ):
+class DictPropertyFactory( object ):
 
+  ASdQc={ 'Object':pynav.Pynav(),
+          'url':'http://www.sq.gouv.qc.ca/personnes-disparues/personnes-disparues-surete-du-quebec.jsp#pageDemandee=1',
+          'web':{
+            'image':None,
+            'link':None } }
+
+  IntNodeLevel=0
+  CurrentNodeLevel=None
+  TreeNode=[ ] 
+
+  RootNode=None
+  @DecoratorSQ.GlobalKeyNameAssertion( __KeyDictAttribute__ )
+  def GetRootNode( self ):
+    return self.RootNode
+
+  @DecoratorSQ.GlobalKeyNameAssertion( __KeyDictAttribute__ )
+  def SetRootNode( self, value ):
+    self.RootClassName, self.RootClassNode = value
+    self.RootNode=getattr( self.RootClassName, self.RootClassNode )
+  
+  PropertyRootNode=property( GetRootNode, SetRootNode)
+  
+  ParentKey=None
+  @DecoratorSQ.GlobalKeyNameAssertion( __KeyDictAttribute__ )
   def GetParentKey( self ):
-    return self.RootKey
+    return self.ParentKey
 
+  @DecoratorSQ.GlobalKeyNameAssertion( __KeyDictAttribute__ )
   def SetParentKey( self, value ):
     Value = value
-    self.RootKey=self.DictReferences[ Value ]
+    self.ParentKey=self.DictReferences[ Value ]
 
+  PropertyParentKey=property( GetParentKey, SetParentKey)
+  
+  ChildKey=None
+  @DecoratorSQ.GlobalKeyNameAssertion( __KeyDictAttribute__ )
   def GetChildKey( self ):
-    return self.RootKey
+    return [self.ChildKey]
 
+  @DecoratorSQ.GlobalKeyNameAssertion( __KeyDictAttribute__ )
   def SetChildKey( self, value ):
     Value = value
-    self.RootKey=self.DictReferences[ Value ]
-    
+    self.ChildKey=self.DictReferences[ Value ]
+
+  PropertyChildKey=property( GetChildKey, SetChildKey )
+  
 class PersonnesDisparues( object ):
 
-  ASdQc={ Object:pynav.Pynav(),
-          url:'http://www.sq.gouv.qc.ca/personnes-disparues/personnes-disparues-surete-du-quebec.jsp#pageDemandee=1',
-          web:{
-            'image':None,
-            link:None } }
+  Factory=DictPropertyFactory()
 
-
-  def __init__( self, **Kargs ):
+  @DecoratorSQ.InstanceFuncMessage( "Creating Instance" )
+  def InitCall( self ):
+    return True
     
+  @DecoratorSQ.ParseTheKargs( Factory , None, None )
+  def __init__( self, **Kargs ):
+    self.InitCall( )
